01e06453a705b6fa2cfd9f7c113aac4888de6ac2
unknown <tientriendinh224@gmail.com>
2025-10-21 18:06:23 +0700

    lab02

---
+++ Day01/src/main/java/com/tientriendinh/lesson01_spring_boot/Lesson01SpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.tientriendinh.lesson01_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson01SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson01SpringBootApplication.class, args);
+        System.out.println("Dinh trien - 2310900107");
+	}
+
+}
---
+++ Day01/src/test/java/com/tientriendinh/lesson01_spring_boot/Lesson01SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tientriendinh.lesson01_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson01SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
